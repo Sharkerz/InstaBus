d726c47660650ec61800d70471628ef56810b515
Benoit Diemer <benoit.diemer@supinfo.com>
2021-01-18 18:20:54 +0100

    Carte

--- app/src/main/java/com/example/instabus/ui/Map/MapFragment.kt
+++ app/src/main/java/com/example/instabus/ui/Map/MapFragment.kt
@@ -1,21 +1,71 @@
 package com.example.instabus.ui.Map
 
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.example.instabus.MainActivity
 import com.example.instabus.R
+import com.example.instabus.Station
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.activity_station_detail.*
+import kotlinx.android.synthetic.main.fragment_map.*
 
-class MapFragment : Fragment() {
+
+class MapFragment : Fragment(), OnMapReadyCallback{
+  companion object {
+    private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+  }
 
   private lateinit var dashboardViewModel: MapViewModel
+  private lateinit var googleMap : GoogleMap
+  private val Barcelone = LatLng(41.405049, 2.163203)
+  private lateinit var listStation: List<Station>;
+  private lateinit var lastLocation: Location
+  private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+  override fun onActivityCreated(savedInstanceState: Bundle?) {
+    super.onActivityCreated(savedInstanceState)
+    map_view.onCreate(savedInstanceState)
+    map_view.onResume()
+
+    map_view.getMapAsync(this)
+    getDataStation()
+    fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+  }
+
+  override fun onMapReady(map: GoogleMap?) {
+    if(map != null){
+      googleMap = map
+    }
+    for(station in listStation){
+      googleMap.addMarker(
+        MarkerOptions()
+          .position(LatLng(station.lat.toDouble(),station.lon.toDouble()))
+          .title(station.street_name)
+      )
+    }
+    googleMap.uiSettings.isZoomControlsEnabled = true
+    googleMap.uiSettings.isMapToolbarEnabled = false
+    googleMap.uiSettings.isMyLocationButtonEnabled = true
+    googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Barcelone,17f))
+    setUpMap()
+
+  }
 
   override fun onCreateView(
-
     inflater: LayoutInflater,
     container: ViewGroup?,
     savedInstanceState: Bundle?
@@ -23,11 +73,33 @@ class MapFragment : Fragment() {
     dashboardViewModel =
             ViewModelProvider(this).get(MapViewModel::class.java)
     val root = inflater.inflate(R.layout.fragment_map, container, false)
-    val textView: TextView = root.findViewById(R.id.text_dashboard)
-    dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
-      textView.text = it
-    })
-
     return root
   }
+
+  fun getDataStation() {
+    val activity: MainActivity? = activity as MainActivity?
+    listStation = activity?.getStationList()!!
+  }
+
+  private fun setUpMap() {
+    if (ActivityCompat.checkSelfPermission(requireActivity(),
+        android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+      ActivityCompat.requestPermissions(requireActivity(),
+        arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+      return
+    }
+    googleMap.isMyLocationEnabled = true
+
+    fusedLocationClient.lastLocation.addOnSuccessListener(requireActivity()) { location ->
+
+      if (location != null) {
+        lastLocation = location
+        val currentLatLng = LatLng(location.latitude, location.longitude)
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 17f))
+      }
+    }
+  }
+
+
+
 }
